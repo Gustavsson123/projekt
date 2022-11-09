19d4a55b5944fd02b2ae3bf1c285d256b3c44b74
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2022-11-09 10:23:34 +0100

    PROJECT

---
+++ FPS Project/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera cam;
+    private float xRotation = 0f;
+
+    public float xSensitivity = 10f;
+    public float ySensitivity = 10f;
+
+    public void ProcessLook(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
+    }
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
