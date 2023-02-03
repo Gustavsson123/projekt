45c45c15114ae7fdd179e266d495a594f9dd47e1
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2023-02-03 10:39:31 +0100

    Small changes to location array

--- map-test/src/App.js
+++ map-test/src/App.js
@@ -1,29 +1,23 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
 // import * as parkData from "./data/skateboard-parks.json";
-import L from 'leaflet';
 
 function App() {
   const positions = [
-    { long: 59.840236, lat: 17.650246 },
-    { long: 17.840236, lat: 12.450246 },
-    { long: 51.840236, lat: 19.650246 },
-    { long: 69.840236, lat: 16.650246 },
-    { long: 53.840236, lat: 120.650246 },
-    { long: 39.027871, lat: 125.707641 }
+    { long: 59.840236, lat: 17.650246, info: "Nti Uppsala" },
+    { long: 17.840236, lat: 12.450246, info: "Lorem" },
+    { long: 51.840236, lat: 19.650246, info: "Lorem3" },
+    { long: 69.840236, lat: 16.650246, info: "Lorem5" },
+    { long: 53.840236, lat: 120.650246, info: "Test" },
+    { long: 39.027871, lat: 125.707641, info: "Nordkorea" }
   ]
-
-  const customIcon = new L.Icon({//creating a custom icon to use in Marker
-    iconUrl: "http://www.clipartbest.com/cliparts/di7/ppB/di7ppBG9T.png",
-    iconSize: [25, 25]
-  });
 
   const getAllPins = () => {
     return positions.map((pos, index) => {
       return (
-        <Marker key={index} position={[pos.long, pos.lat]} icon={customIcon}>
+        <Marker key={index} position={[pos.long, pos.lat]}>
           <Popup>
-            Burma Benny
+            {pos.info}
           </Popup>
         </Marker>
       )
